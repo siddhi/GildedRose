9addccf7dfffc9893f63db05ae7c8757ac3d548d
Siddharta Govindaraj <siddharta@silverstripesoftware.com>
2019-06-14 19:11:44 +0530

    Inverted negation in conditional of updateQualityOfExpiredItem and converted to if-elseif

--- src/main/java/com/gildedrose/GildedRose.java
+++ src/main/java/com/gildedrose/GildedRose.java
@@ -18,20 +18,18 @@ class GildedRose {
     }
 
 	private void updateQualityOfExpiredItem(Item item) {
-		if (!item.name.equals("Aged Brie")) {
-		    if (!item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-		        if (item.quality > 0) {
-		            if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
-		                item.quality = item.quality - 1;
-		            }
-		        }
-		    } else {
-		        item.quality = item.quality - item.quality;
-		    }
-		} else {
+		if (item.name.equals("Aged Brie")) {
 		    if (item.quality < 50) {
 		        item.quality = item.quality + 1;
 		    }
+		} else if (item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+		        item.quality = item.quality - item.quality;
+	    } else if (item.name.equals("Sulfuras, Hand of Ragnaros")) {
+        }
+        else {
+        	if (item.quality > 0) {
+                item.quality = item.quality - 1;
+            }
 		}
 	}
 
