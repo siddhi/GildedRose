413386b2de8a5c34dc638158da6a9835f37601b2
Siddharta Govindaraj <siddharta@silverstripesoftware.com>
2019-06-15 18:04:49 +0530

    Pinned Sulfuros quality to 80

--- src/main/java/com/gildedrose/items/Sulfuras.java
+++ src/main/java/com/gildedrose/items/Sulfuras.java
@@ -1,8 +1,11 @@
 package com.gildedrose.items;
 
+import com.gildedrose.types.BoundedCounter;
+
 public class Sulfuras extends Item {
-	public Sulfuras(int sellIn, int quality) {
-		super("Sulfuras, Hand of Ragnaros", sellIn, quality);
+	public Sulfuras(int sellIn) {
+		super("Sulfuras, Hand of Ragnaros", sellIn, 80);
+		quality = new BoundedCounter(80, 80, 80);
 	}
 
 	@Override
